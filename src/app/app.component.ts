c50bd3cb5cb00aebd9217c3cd26474e05e14ac47
LucasCelestino <lucasrashido@gmail.com>
2022-03-25 20:58:27 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { AnimationKeyframesSequenceMetadata } from '@angular/animations';
 import { Component, VERSION } from '@angular/core';
+import { isEmpty } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  tarefas: String[] = [];
+
+  tarefaInput: string;
+
+  adicionarTarefa() {
+    if (this.tarefaInput != null) {
+      console.log(this.tarefas);
+    } else {
+      alert('preencha o campo antes de continuars');
+    }
+  }
 }
